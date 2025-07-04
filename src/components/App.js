6de76d5a5d87af23e003e8084b980015ccb920bb
= <=>
2025-07-04 22:01:00 +0530

    First commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import '../styles/App.css';
-import Selection from './Selection';
-import ColourSelector from './ColourSelector';
+import Selection from './Selection';              
+import ColourSelector from './ColourSelector';  
+
 
 const colourConfig = [{
     key: 'blue',
@@ -31,7 +32,7 @@ const App = () => {
 
   return (
     <div id="master">
-      <h5 className="heading">{/* display title here */}</h5>
+      <h5 className="heading">{title}</h5>
 
       <div className="row">
         {colourConfig.map((config, index) => (
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,18 @@
 
+import React, { useState } from 'react';
+
+function Selection({ applyColor }) {
+  const [bgStyle, setBgStyle] = useState({});
+
+  const handleClick = () => {
+    applyColor(setBgStyle);
+  };
+
+  return (
+    <div className="fix-box" style={bgStyle} onClick={handleClick}>
+      <h5 className="subheading">Selection</h5>
+    </div>
+  );
+}
+
+export default Selection;
